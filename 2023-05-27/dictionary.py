a77c33809494ed55f0709a77ca6f61ca4e2f77c7
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-05-27 15:58:36 +0300

    Python dictionary

---
+++ 2023-05-27/dictionary.py
@@ -0,0 +1,17 @@
+city = {
+	"Kenya" : "Nairobi",
+	"Uganda": "Kampala",
+	"Australia": "Sydney",
+	"Netherlands": "Amsterdam"
+}
+
+print(city["Netherlands"])
+
+# Using the dict() constructor to make a dictionary
+
+info = dict(
+		name = 'Arthur',
+		reg = 'ICT-G-4-0958-19',
+		year = 2019
+	   )
+print(info)
