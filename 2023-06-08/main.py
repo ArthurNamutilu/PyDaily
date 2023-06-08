17b4d1e6e0ae99ff70a850f3c218ee5f996a19f9
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-06-08 21:17:42 +0300

    basic py modules

---
+++ 2023-06-08/main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import module
+
+print("This line always execute when main.py is run")
+module.hello()
---
+++ 2023-06-08/module.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def hello():
+    print("Hello from module.py!")
+
+print("This line always executes when module.py is imported.")
