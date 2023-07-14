bd34eee9970e6662a13c1b4f78a7f0ea6c6d2b2e
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-07-14 23:09:42 +0300

    simple madlibs game

---
+++ 2023-07-14/madLibs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+color = input("What is your favorite color?: ")
+pluralNoun = input("Which plural noun are you thinking about now?: ")
+celeb = input("Who is your favorite celebrity?: ")
+
+print(f"Roses are {color}")
+print(f"{pluralNoun} are blue")
+print(f"I love {celeb}")
