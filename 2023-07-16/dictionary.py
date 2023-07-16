0661fec14e05b0190cabd66f740c1eca8a18f78f
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-07-16 12:53:19 +0300

    python dictionary

---
+++ 2023-07-16/dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+my_dict = {}
+my_dict['name'] = "Arthur Fred"
+my_dict['favColor'] = 'Green'
+
+dict2 = {"School": "Computing", "code": 258}
+
+print(my_dict['favColor'])
+print(my_dict.keys())
+print(dict2.values())
