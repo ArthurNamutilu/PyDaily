89bc707e8d92c230d350a969bae874654e70bfc9
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-07-19 20:17:44 +0300

    simple translator

--- 2023-07-19/simpleTranslator.py
+++ 2023-07-19/simpleTranslator.py
@@ -2,11 +2,14 @@
 
 phrase = input("Enter phrase to translate: ")
 def translate(phrase):
-    vowels = "AEIOUaeiou"
+    vowels = "aeiou"
     translation = ""
     for letter in phrase:
-        if letter in vowels:
-            translation = translation + 'g'
+        if letter.lower() in vowels:
+            if letter.isupper():
+                translation = translation + 'G'
+            else:
+                translation = translation + 'g'
         else:
             translation = translation + letter
     return translation
