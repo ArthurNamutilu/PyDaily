bb428476c0d424074566371bc6e602931d27c585
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-07-21 12:40:16 +0300

    writing to file

--- 2023-07-20/write-file.py
+++ 2023-07-20/write-file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
-my_file = open("speciality.txt", 'r')
+my_file = open("speciality.txt", 'a')
 
-print(my_file.read())
+# my_file.write('\n')
+my_file.write("Arthur Fred Namutilu - Software Engineer")
+my_file.write('\n')
+
+# print(my_file.read())
+my_file.close
