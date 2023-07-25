3fe3daf03ef3d2e850f9f07975f0826e67502d8b
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-07-25 12:56:07 +0300

    modifying collecton while iterating it

---
+++ 2023-07-25/index.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+members = {
+	"Arthur": "Present",
+	"Erickson": "Absent",
+	"Gg": "Present",
+	"Fred": "Absent",
+	"Programmer": "Present",
+}
+
+for user, status in members.copy().items():
+    if status == 'Absent':
+        del members[user]
+
+active_members = {}
+for user, status in members.items():
+    if status == 'Present':
+        active_members[user] = status
+print(active_members)
