fff9f5a6e7ed7f2ea9ebfd675850f80b73e2486c
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-08-10 16:34:08 +0300

    Use of __name__

---
+++ 2023-08-10/calc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+if __name__ == "__main__":
+    print("Running tests for the calculator module:")
+    print(add(5, 3))
+    print(subtract(10, 7))
