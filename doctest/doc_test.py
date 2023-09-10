bf38a7f97b66f47947fd34b3b04955d3c32435cb
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-09-10 23:29:10 +0300

    test file 2

---
+++ doctest/doc_test.py
@@ -0,0 +1,20 @@
+def mul(a, b):
+    """
+    Function that multiplies two numbers.
+
+    :param a: First digit
+    :param b: Second digit
+    :return: product of a and b
+
+    >>> mul(2, 6)
+    12
+    >>> mul(-1, 8)
+    -8
+    >>> mul(7, 0)
+    0
+    """
+    return a * b
+
+
+net_pay = mul(3820, 141)
+print(f"The net pay is {net_pay}")
