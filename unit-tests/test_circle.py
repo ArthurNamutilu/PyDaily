74cec7e5ad3506015ec038e496f713745eb903f7
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-09-13 21:36:52 +0300

    unit test

--- unit-tests/test_circle.py
+++ unit-tests/test_circle.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 import unittest
 from circle import circle_area
 from math import pi
+
 
 class TestCircleArea(unittest.TestCase):
     def test_area(self):
@@ -10,12 +11,10 @@ class TestCircleArea(unittest.TestCase):
         self.assertAlmostEqual(circle_area(0), 0)
         self.assertAlmostEqual(circle_area(2.1), pi * 2.1**2)
 
-
     def test_values(self):
         """ Raises value error whenever necessary """
         self.assertRaises(ValueError, circle_area, -2)
-
-
+    
     def test_types(self):
         """ Ensures r is a number """
         self.assertRaises(TypeError, circle_area, 3+5j)
