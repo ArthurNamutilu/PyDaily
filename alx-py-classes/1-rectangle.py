a4de66e45b8af69134040808f57c9ce41340f019
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-10-04 20:08:14 +0300

    class

---
+++ alx-py-classes/1-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that creates a class Rectangle"""
+
+
+class Rectangle:
+    """Class that defines a rectangle"""
+    def __init__(self, width):
+        """initializing width"""
+        self._width = width
+
+    def width(self):
+        self.width = 
