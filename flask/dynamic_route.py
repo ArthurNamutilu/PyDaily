1d0fe4ad9df48372b75ee0f3b2716d6b0006ba10
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-10-09 21:26:11 +0300

    Dynamic route

---
+++ flask/dynamic_route.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')  # static route
+def index():
+    return 'Home'
+
+
+""" Dynamic route """
+
+
+@app.route('/hello/<string:name>/')
+def post(name):
+    return f'hello from {name}'
+
+
+"""Dynamic route with two optional parameters"""
+
+
+@app.route('/hello/<name>/<age>/')
+def getpost(name, age):
+    return f'hello from {name} who is {age}'
+
+
+app.run(host='0.0.0.0', port=5000)
