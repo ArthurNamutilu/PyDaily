66f60bc67a2478d7cab21380e2ae68fcb57592f9
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-10-10 22:11:43 +0300

    my routes

--- flask/main.py
+++ flask/main.py
@@ -1,6 +1,24 @@
-from website import create_app
+from flask import Flask
 
-app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Home'
+
+
+@app.route('/hello')
+def helloIndex():
+    return 'hello from flask'
+
+
+app.run(host='0.0.0.0', port=5000)
+
+# from website import create_app
+
+# app = create_app()
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
