539af1ef40950debe6344975eb3ce7b2e1ebcd98
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-10-16 23:45:28 +0300

    rectanle class

--- more_classes/1-rectangle.py
+++ more_classes/1-rectangle.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 """ Rectangle class"""
-
-
 class Rectangle:
-    def __int__(self, value):
-        self._value = value
+    """ rectangle class with width attribute """
+    def __int__(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
 
     @property
+    def width(self):
+        return self.__width
+
+    @width.setter
     def width(self, value):
-        return self._value
-
-    def width(self, new_value):
-        self._value = new_value
-
-    
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
