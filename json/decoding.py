8c005fc2184ee9d7653a423744e0e7947dc9e0ee
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-12-16 15:54:56 +0300

    -mdeserialization

--- json/decoding.py
+++ json/decoding.py
@@ -2,5 +2,5 @@
 import json
 
 encoded_data = '{"name": "John", "age": 30, "city": "New York"}'
-dencoded_data = json.loads(encoded_data)
-print(dencoded_data)
+decoded_data = json.loads(encoded_data)
+print(decoded_data)
