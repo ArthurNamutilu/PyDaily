3d4357937ec5f19b290292ec1958e5f96650e08f
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-01-20 23:18:20 +0300

    -mdatabase connection

--- learn_flask/app.py
+++ learn_flask/app.py
@@ -1,7 +1,34 @@
 #!/usr/bin/python3
 from flask import Flask, render_template
+import os
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Set the base directory
+base_dir = os.path.abspath(os.path.dirname(__file__))
+DB_NAME = "data.db"
+# configuring the flask application for SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# creating a SQLAlchemy instance
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), unique=True, nullable=False)
+    email = db.Column(db.String(40), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+# initialising the database
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
@@ -12,6 +39,15 @@ def hello_world():
 @app.route('/user/<username>')
 def show_user(username):
     return f'The user is {username}'
+
+
+# route to interact with database
+@app.route('/add_user/<username>/<email>')
+def add_user(username, email):
+    new_user = User(username=username, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+    return f"User {username} added!"
 
 
 if __name__ == '__main__':
